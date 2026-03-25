00eae091926f1ad5ca8fd64a7ab2689a15dcb023
JP <30578360+PeteJuli@users.noreply.github.com>
2026-03-25 10:13:40 +0100

    Update main.cpp

--- SRC/main.cpp
+++ SRC/main.cpp
@@ -4,11 +4,6 @@
 #include <cstdint>
 #include "Driver/Mouse/MouseDriver.hpp"
 
-//Hardcode...
-int dpi = 800;
-int mainColor[4] = {255, 0, 0, 100};//RGBA
-int PollingRate = 3; //0=125HZ, 1=250Hz, 2=500Hz, 3=1000Hz
-
 int main()
 {
     MouseDriver mouse;
